e2e38428fd173c92e87b072f3fb6f07abe76a8ac
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-12-12 15:15:40 +0100

    FIX: env.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.index');
-});
+Route::get('/', [PageController]);
+
+// function () {
+//     return view('guest.index');
+// });
